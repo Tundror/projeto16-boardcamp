89e39ae482e190e881ad75a7f8e47be94998b8d0
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-05-12 04:33:11 -0300

    rota para inserir novo jogo completa

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -1,12 +1,28 @@
 import { db } from "../database/database.connection.js";
 
-export async function getGames(req, res){
-    try{
+export async function getGames(req, res) {
+    try {
         const games = await db.query(`SELECT * FROM games;`)
         console.table(games.rows)
         res.send(games.rows)
     }
-    catch(err){
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function insertGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body
+    if (stockTotal <= 0 || pricePerDay <= 0) return sendStatus(400)
+    try {
+        const checkName = await db.query(`SELECT * FROM games WHERE name=$1`, [name])
+        if (checkName.rows.length !== 0) return res.sendStatus(409)
+        await db.query(`insert INTO games (name, image, "stockTotal", "pricePerDay") 
+        VALUES ($1, $2, $3, $4)`,
+            [name, image, stockTotal, pricePerDay])
+
+        res.sendStatus(201)
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getGames } from "../controllers/game.controllers.js";
+import { getGames, insertGame } from "../controllers/game.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { gameSchema } from "../schemas/games.schemas.js";
 
 const gameRouter = Router()
 
-gameRouter.get("/games",  getGames)
+gameRouter.get("/games", getGames)
+gameRouter.post("/games", validateSchema(gameSchema), insertGame)
 
 export default gameRouter
