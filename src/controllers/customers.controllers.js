e3e5f9c0eabf80ebb25b42a1e2d31ef249f615a5
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-05-12 06:06:59 -0300

    rota get costumer by id

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -3,12 +3,34 @@ import { db } from "../database/database.connection.js";
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT * FROM customers;`)
+
         const formattedCustomers = customers.rows.map(customer => {
             const formattedBirthday = customer.birthday.toISOString().substring(0, 10);
             return { ...customer, birthday: formattedBirthday };
         });
+
         console.table(formattedCustomers)
         res.send(formattedCustomers)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getCustomersById(req, res) {
+    const id = parseInt(req.params.id)
+    try {
+        const customers = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+
+        if(customers.rows.length === 0) return res.sendStatus(404)
+
+        const formattedCustomers = customers.rows.map(customer => {
+            const formattedBirthday = customer.birthday.toISOString().substring(0, 10);
+            return { ...customer, birthday: formattedBirthday };
+        });
+
+        console.log(formattedCustomers[0])
+        res.send(formattedCustomers[0])
     }
     catch (err) {
         res.status(500).send(err.message)
@@ -21,6 +43,7 @@ export async function insertCustomer(req, res) {
     try {
         const checkCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])
         if (checkCpf.rows.length !== 0) return res.sendStatus(409)
+
         await db.query(`insert INTO customers (name, phone, cpf, birthday) 
         VALUES ($1, $2, $3, $4)`,
             [name, phone, cpf, birthday])
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getCustomers, insertCustomer } from "../controllers/customers.controllers.js";
+import { getCustomers, getCustomersById, insertCustomer } from "../controllers/customers.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { customerSchema } from "../schemas/customers.schemas.js";
 
 const customerRouter = Router()
 
 customerRouter.get("/customers", getCustomers)
+customerRouter.get("/customers/:id", getCustomersById)
 customerRouter.post("/customers", validateSchema(customerSchema), insertCustomer)
+
 
 export default customerRouter
